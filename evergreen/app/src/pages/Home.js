af4c35907f9a185ecd9ce4a4276f1abfd1a2650f
Achille <58356637+SharkBoy69-CS@users.noreply.github.com>
2022-11-27 17:31:34 +0100

    Update Home.js

--- evergreen/app/src/pages/Home.js
+++ evergreen/app/src/pages/Home.js
@@ -6,24 +6,24 @@ import Username from "../components/Username";
 
 import { ReactComponent as Github } from "../assets/icons/svg/github.svg";
 import { ReactComponent as Code } from "../assets/icons/svg/code.svg";
+import { ReactComponent as Youtube } from "../assets/icons/svg/youtube.svg";
 import { ReactComponent as OF } from "../assets/icons/svg/onlyfans.svg";
 
 
 function Home() {
   return (
     <>
-      <Header profile="https://avatars.githubusercontent.com/u/44700383?v=4"/>
+      <Header profile="https://avatars.githubusercontent.com/u/58356637?s=400&u=3c76f657b46dd5adb9bf0bd92e495ddbd42aac52&v=4"/>
 
-      <Username text="neo"/>
+      <Username text="SharkBoy"/>
 
-      <Welcome text="Hi, I am neo"/>
+      <Welcome text="Hi, I am SharkBoy !"/>
       <Description
-        text="I am a developer. Feel free to use my stuff"
+        text="I am trying to dev 😅"
       />
-
       <Button
         text="Github"
-        link="https://github.com/Neotoxic-off"
+        link="https://github.com/SharkBoy69-CS"
         icon={Github}
       />
       <Button
@@ -31,9 +31,15 @@ function Home() {
         link="https://github.com/Neotoxic-off/evergreen"
         icon={Code}
       />
+      />
+      <Button
+        text="Youtube"
+        link="https://www.youtube.com/channel/UCkz62-16Nsw-3Jd7qGqM3pw"
+        icon={Youtube}
+      />
       <Button
         text="Onlyfans"
-        link="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        link="https://youtu.be/Pwq4rqJYH8o"
         icon={OF}
       />
     </>
